a6be075a7bd759ef3ec6c29ae447c96d070c53b7
lollipop <liujian594@hotmail.com>
2023-10-10 22:44:15 +0800

    电脑Bug了，先折腾电脑

---
+++ ApkFileRegistry.cs
@@ -0,0 +1,11 @@
+﻿namespace ApkHelper;
+
+public class ApkFileRegistry
+{
+    // 晚上抄了一段代码，尝试写注册表，但是感觉不对劲
+    // https://www.cnblogs.com/kybs0/p/5772321.html
+    // string toolPath = @"D:\Gitee\WhiteBoard\Code\bin\Debug\WhiteBoard.exe";
+    // string extension = ".iwb";
+    // string fileType = "White Board File";
+    // string icon = @"D:\Gitee\WhiteBoard\Code\bin\Debug\fileIcon.ico";
+}
